8c1c48b4d73a77a01007aede5a9a9dd172bd5986
ZiaulKarim245 <ziaulkarim278@gmail.com>
2026-07-12 16:52:47 +0600

    Add Email Notification Channel.

---
+++ src/Concretes/EmailMessage.php
@@ -0,0 +1,39 @@
+<?php
+    namespace App\Concrete;
+
+    use App\Abstracts\BaseMessage;
+    use App\Interfaces\NotificationChannelInterface;
+    use App\Traits\Loggable;
+
+    /**
+     * Email notification channel execution.
+     */
+    class EmailMessage extends BaseMessage implements NotificationChannelInterface{
+        use Loggable;
+
+        private string $subject;
+
+        public function __construct(string $recipient, string $subject, string $message) {
+            parent::__construct($recipient,$message);
+            $this->subject = $subject;
+        }
+
+        protected function formatMessage(): string {
+            return "To: {$this->getRecipient()} \nSubject: {$this->subject} \nBody: {$this->getMessage()}";
+        }
+
+        public function send(): bool {
+            $fomatted = $this->formatMessage();
+            $result = true; # pretend it works because of not having real service provider
+
+            // $response = file_get_contents("https://sendgrid.com...", $formatted);
+            // $result = ($response === "200 OK"); // True only if the internet server says yes
+
+            $this->logDelivery("Email", $this->getRecipient(), $result);
+            return $result;
+        }
+
+        public function __toString(): string {
+            return "Email successfully send to {$this->getRecipient()}";
+        }
+    }
