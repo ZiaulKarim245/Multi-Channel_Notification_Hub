f802fd1bde9ad02a9a16cde439995a3ff0bd867c
ZiaulKarim245 <ziaulkarim278@gmail.com>
2026-07-12 21:11:46 +0600

    Add Firebase Push Notification Channel.

---
+++ src/Concretes/PushMessage.php
@@ -0,0 +1,40 @@
+<?php
+    namespace App\Concretes;
+
+    use App\Abstracts\BaseMessage;
+    use App\Interfaces\NotificationChannelInterface;
+    use App\Traits\Loggable;
+
+    /**
+     * Firebase Web Push Notification Channel execution.
+     */
+
+    class PushMessage extends BaseMessage implements NotificationChannelInterface {
+        use Loggable;
+
+        private string $firebaseToken;
+
+        public function __construct(string $recipient, string $message, string $firebaseToken) {
+            parent::__construct($recipient, $message);
+
+            $this->firebaseToken = $firebaseToken;
+        }
+
+        public function setFirebaseToken(string $firebaseToken): void {
+            $this->firebaseToken = $firebaseToken;
+        }
+
+        protected function formatMessage(): string {
+            return "Token: $this->firebaseToken\nMessage: " . $this->getMessage();
+        }
+
+        public function send(): bool {
+            $result = true;
+            $this->logDelivery("Firebase Push", $this->getRecipient(), $result);
+            return $result;
+        }
+
+        public function __toString(): string {
+            return "Push Notification sent to ". $this->getRecipient();
+        }
+    }
